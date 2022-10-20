2fd06a29fd28c65f6efbc51a168e964411fca0f2
özkan demir <ozkanndemir2009@gmail.com>
2022-10-21 00:49:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,110 @@
+import React, { useState,useEffect } from "react";
+import mailSvg from "./assets/mail.svg";
+import manSvg from "./assets/man.svg";
+import womanSvg from "./assets/woman.svg";
+import manAgeSvg from "./assets/growing-up-man.svg";
+import womanAgeSvg from "./assets/growing-up-woman.svg";
+import mapSvg from "./assets/map.svg";
+import phoneSvg from "./assets/phone.svg";
+import padlockSvg from "./assets/padlock.svg";
+import cwSvg from "./assets/cw.svg";
+import Footer from "./components/footer/Footer";
+import axios from "axios";
+
+const url = "https://randomuser.me/api/";
+const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
+
+function App() {
+  const [user, setUser] = useState("");
+ 
+
+  const getUsers = async () => {
+    try {
+      const { data } = await axios(url);
+      console.log(data.results[0]);
+      setUser(data.results[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+  const {id,picture,email,name,location,dob}=user
+  console.log(user)
+   const [changeuser, setChangeuser] = useState("name")
+   const [changeparagraf, setChangeparagraf] = useState(name)
+   
+
+  return (
+    <main>
+      <div className="block bcg-orange">
+        <img src={cwSvg} alt="cw" id="cw" />
+      </div>
+      <div className="block">
+        <div className="container">
+          <img src={picture?.large} alt="random user" className="user-img" />
+          <p className="user-title">My {changeuser} is </p>
+          <p className="user-value">
+            
+            
+             </p>
+          <div className="values-list">
+            <button className="icon" data-label="name"
+             onClick={()=> setChangeuser("name") } >
+              <img src={womanSvg}  alt="user" id="iconImg" />
+            </button>
+            <button className="icon" data-label="email"
+                onClick={()=> setChangeuser("email") } >
+              <img src={mailSvg} alt="mail" id="iconImg" />
+            </button>
+            <button className="icon" data-label="age"
+              onClick={()=> setChangeuser("age") } >
+              <img src={womanAgeSvg} alt="age" id="iconImg" />
+            </button>
+            <button className="icon" data-label="street"
+               onClick={()=> setChangeuser("street") } >
+              <img src={mapSvg} alt="map" id="iconImg" />
+            </button>
+            <button className="icon" data-label="phone"
+               onClick={()=> setChangeuser("phone") } >
+              <img src={phoneSvg} alt="phone" id="iconImg" />
+            </button>
+            <button className="icon" data-label="password"
+              onClick={()=> setChangeuser("password") }>
+              <img src={padlockSvg} alt="lock" id="iconImg" />
+            </button>
+          </div>
+          <div className="btn-group">
+            <button className="btn" type="button">
+              new user
+            </button>
+            <button className="btn" type="button">
+              add user
+            </button>
+          </div>
+
+          <table className="table">
+            <thead>
+              <tr className="head-tr">
+                <th className="th">Firstname</th>
+                <th className="th">Email</th>
+                <th className="th">Phone</th>
+                <th className="th">Age</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="body-tr"></tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Footer />
+      </div>
+    </main>
+  );
+}
+
+export default App;
